5ce5ab53e23c4049b5da7480a30baea235d2cbd1
YuryRazhkov <yrozhkov1983@gmail.com>
2021-11-23 07:18:15 +0300

    lesson_2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render
 
+links_menu = [
+    {'href': 'products', 'name': 'Все'},
+    {'href': 'products_home', 'name': 'Дом'},
+    {'href': 'products_modern', 'name': 'Модерн'},
+    {'href': 'products_office', 'name': 'Офис'},
+    {'href': 'products_classic', 'name': 'Классика'},
+]
+
 
 def index(request):
     return render(request, 'mainapp/index.html')
 
-
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
 
+    return render(request, 'mainapp/products.html', context)
+
+def products_home(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+
+    return render(request, 'mainapp/products.html', context)
+
+def products_modern(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+
+    return render(request, 'mainapp/products.html', context)
+
+def products_office(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+
+    return render(request, 'mainapp/products.html', context)
+
+def products_classic(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+
+    return render(request, 'mainapp/products.html', context)
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
