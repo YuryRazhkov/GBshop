cfccc30891c0c3bb24252cb450387a75818b06ba
YURY <yrozhkov1983@gmail.com>
2021-12-13 10:23:06 +0300

    lesson-7

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from adminapp import views as adminapp
 
+app_name = 'adminapp'
+
 urlpatterns = [
     path('users/', adminapp.users, name='users'),
     path('users/create/', adminapp.user_create, name='user_create'),
@@ -9,15 +11,15 @@ urlpatterns = [
 
     path('categories/', adminapp.categories, name='categories'),
     path('categories/create/', adminapp.category_create, name='category_create'),
-    path('categories/update/<int:pk>/', adminapp.category_create, name='category_update'),
-    path('categories/delete/<int:pk>/', adminapp.category_create, name='category_delete'),
+    path('categories/update/<int:pk>/', adminapp.category_update, name='category_update'),
+    path('categories/delete/<int:pk>/', adminapp.category_delete, name='category_delete'),
 
-    path('products/', adminapp.categories, name='products'),
+    path('products/<int:pk>/', adminapp.products, name='products'),
     path('products/create/', adminapp.product_create, name='product_create'),
     path('products/update/<int:pk>/', adminapp.product_update, name='product_update'),
     path('products/delete/<int:pk>/', adminapp.product_delete, name='product_delete'),
     path('products/read/<int:pk>/', adminapp.product_read, name='product_read'),
 
 ]
-app_name = 'adminapp'
+
 
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,77 +1,125 @@
-from django.shortcuts import render
 
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from adminapp.forms import ShopUserAdminEditForm
+from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
-from mainapp.models import Product
+from mainapp.models import Product, ProductCategory
 
-
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     context = {
         'object_list': ShopUser.objects.all().order_by('-is_active')
     }
     return render(request, 'adminapp/users_list.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_create(request):
-    context = {
 
-    }
-    return render(request, 'adminapp/users_list.html', context)
+        if request.method == 'POST':
+            user_form = ShopUserRegisterForm(request.POST, request.FILES)
+            if user_form.is_valid():
+                user_form.save()
+                return HttpResponseRedirect(reverse('adminapp:users'))
+        else:
+            user_form = ShopUserRegisterForm()
 
+        context = {
+            'form': user_form
+        }
+        return render(request, 'adminapp/users_form.html', context)
+
+@user_passes_test(lambda u: u.is_superuser)
 def user_update(request, pk):
+    curent_user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == 'POST':
+        user_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=curent_user)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('adminapp:users'))
+    else:
+        user_form = ShopUserAdminEditForm(instance=curent_user)
+
     context = {
-
+        'form': user_form
     }
-    return render(request, 'adminapp/users_list.html', context)
+    return render(request, 'adminapp/users_form.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_delete(request, pk):
-    context = {
+    current_user = get_object_or_404(ShopUser, pk=pk)
 
+    if request.method == "POST":
+        current_user.is_active = False
+        current_user.save()
+        return HttpResponseRedirect(reverse('adminapp:users'))
+
+    context = {
+        'object': current_user,
     }
-    return render(request, 'adminapp/users_list.html', context)
+    return render(request, 'adminapp/users_delete.html', context)
 
 
-def categories(request, pk):
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def categories(request):
+
     context = {
 
-    }
-    return render(request, 'adminapp/users_list.html', context)
-
-
-def product(request, pk):
-    context = {
-        'object_list': Product.objects.all()
+        'object_list': ProductCategory.objects.all()
     }
     return render(request, 'adminapp/categories_list.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_create(request):
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('adminapp:users'))
+    else:
+        user_form = ShopUserRegisterForm()
+
     context = {
-
+        'form': user_form
     }
-    return render(request, 'adminapp/users_list.html', context)
+    return render(request, 'adminapp/users_form.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_update(request, pk):
+
     context = {
 
     }
     return render(request, 'adminapp/users_list.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_delete(request, pk):
     context = {
 
     }
     return render(request, 'adminapp/users_list.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def products(request, pk):
     context = {
-        'object_list': Product.objects.filter(category_pk=pk)
+        'object_list': Product.objects.filter(category__pk=pk),
+        'category': ProductCategory.objects.filter(id=pk),
 
     }
     return render(request, 'adminapp/product_list.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_create(request):
     context = {
 
@@ -79,6 +127,7 @@ def product_create(request):
     return render(request, 'adminapp/users_list.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
     context = {
 
@@ -86,12 +135,14 @@ def product_update(request, pk):
     return render(request, 'adminapp/users_list.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_delete(request, pk):
     context = {
 
     }
     return render(request, 'adminapp/users_list.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_read(request, pk):
     context = {
 
